cc9fd720fe21fa0f4e03f63166e7028b231f78b3
Samedov Ehtibar <ekhtibarsamedov666@gmail.com>
2025-03-15 11:08:53 +0300

    Закончил с полями Candidate Regsiter Form и внес пару изменений в Candidate Register Page

--- src/features/candidate/components/forms/CandidateRegisterForm/CandidateRegisterForm.tsx
+++ src/features/candidate/components/forms/CandidateRegisterForm/CandidateRegisterForm.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import AuthForm from '../../../../../components/common/AuthForm/AuthForm';
-import { TextField as MuiTextField, InputAdornment } from '@mui/material';
+import { TextField as MuiTextField, InputAdornment, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { MuiTelInput, classes as MuiTelInputClasses } from 'mui-tel-input'; // Импортируйте MuiTelInput и классы
+import { MuiTelInput, classes as MuiTelInputClasses } from 'mui-tel-input';
 import FlexRowContainer from '../../../../../components/common/FlexContainers/FlexRowContainer';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import FlexColumnContainer from '../../../../../components/common/FlexContainers/FlexColumnContainer';
-import PersonIcon from '@mui/icons-material/Person';
 
 import { default as EmailIcon } from '@mui/icons-material/AlternateEmail';
 import { default as PasswordIcon } from '@mui/icons-material/Password';
@@ -62,34 +61,67 @@ const TextFieldExtraStyle = {
   },
 }
 
-// Стилизация MuiTelInput
-const MUITelInput = styled(MuiTelInput)`
-  & .${MuiTelInputClasses.textField} {
-    font-size: 14px;
-    color: black;
-  }
 
-  .${MuiTelInputClasses.flagButton} {
-    background-color: transparent; 
-  }
+const MUITelInput = styled(MuiTelInput)(({ theme }) => ({
+  '& .MuiInputBase-input': {
+    fontSize: '14px',
+    color: 'black',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#E8E8E8',
+    },
+    '&:hover fieldset': {
+      borderColor: '#3575E2',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#3575E2',
+    },
+  },
+  '& .MuiFormHelperText-root': {
+    color: 'red',
+    fontSize: '12px',
+    marginLeft: '0px',
+    marginBottom: '5px',
+    padding: '0px'
+  },
+  '.MuiTelInput-flagButton': {
+    backgroundColor: 'transparent',
+    padding: '0px'
+  },
+  '.MuiTelInput-flagImg': {
+    width: '18px',
+    padding: '0px'
+  },
+  '.MuiTelInput-callingCode': {
+    color: 'gray',
+  },
+}));
 
-  .${MuiTelInputClasses.flagImg} {
-    width: 20px; 
-  }
+const cities = [
+  'Bakı',
+  'Gəncə',
+  'Sumqayıt',
+  'Lənkəran',
+  'Mingəçevir',
+  'Şirvan',
+  'Şəki',
+  'Lerik',
+  'Quba',
+  'Naftalan',
+];
 
-  .${MuiTelInputClasses.callingCode} {
-    color: gray; 
-  }
+const ITEM_HEIGHT = 36;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 200,
+    },
+  },
+};
 
-  & .MuiOutlinedInput-root {
-    & fieldset {
-      border-color: #E8E8E8;
-    }
-    &.Mui-focused fieldset {
-      border-color: black;
-    }
-  }
-`;
 
 const CandidateRegisterForm = () => {
   const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm<CandidateRegisterFormFields>();
@@ -100,7 +132,7 @@ const CandidateRegisterForm = () => {
   };
 
   return (
-    <AuthForm formTitle='Qeydiyyат' onSubmit={handleSubmit(onSubmit)}>
+    <AuthForm formTitle='Qeydiyyat' onSubmit={handleSubmit(onSubmit)}>
       <FlexColumnContainer>
         <FlexRowContainer gap='10px'>
           <MUITextField
@@ -174,21 +206,71 @@ const CandidateRegisterForm = () => {
           }}
           sx={TextFieldExtraStyle}
         />
-        <MUITelInput
-          {...register("phoneNumber", { required: "Required" })}
-          error={!!errors.phoneNumber}
-          helperText={errors.phoneNumber?.message}
-          variant="outlined"
-          fullWidth
-          placeholder='Phone Number'
-          defaultCountry="AZ"
-          onlyCountries={['AZ']}
-          onChange={(value, info) => {
-            setValue("phoneNumber", value);
-          }}
-          value={watch("phoneNumber")}
-          sx={TextFieldExtraStyle}
-        />
+        <FlexRowContainer gap='10px'>
+          <MUITelInput
+            {...register("phoneNumber", { required: "Required" })}
+            error={!!errors.phoneNumber}
+            helperText={errors.phoneNumber?.message}
+            variant="outlined"
+            fullWidth
+            placeholder='Phone Number'
+            defaultCountry="AZ"
+            onlyCountries={['AZ']}
+            onChange={(value, info) => {
+              setValue("phoneNumber", value);
+            }}
+            value={watch("phoneNumber")}
+            sx={TextFieldExtraStyle}
+          />
+          <FormControl variant="outlined" fullWidth sx={{ ...TextFieldExtraStyle, position: 'relative' }}>
+            <Select
+              {...register("city", { required: "Required" })}
+              error={!!errors.city}
+              defaultValue=""
+              onChange={(event) => setValue("city", event.target.value)}
+              displayEmpty
+              renderValue={(selected) => {
+                if (selected.length === 0) {
+                  return <span style={{ color: '#A0A0A0' }}>Şəhər seçin</span>;
+                }
+                return selected;
+              }}
+              MenuProps={MenuProps}
+              sx={{
+                '& .MuiInputBase-input': {
+                  fontSize: '14px',
+                  color: 'black',
+                },
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#E8E8E8',
+                },
+                '&:hover .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#3575E2',
+                },
+                '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                  borderColor: '#3575E2',
+                },
+                '& .MuiFormHelperText-root': {
+                  color: 'red',
+                  fontSize: '12px',
+                  marginLeft: '0px',
+                  marginBottom: '5px',
+                },
+              }}
+            >
+              {cities.map((city) => (
+                <MenuItem key={city} value={city} sx={{ fontSize: '14px'}}>
+                  {city}
+                </MenuItem>
+              ))}
+            </Select>
+            {errors.city && (
+              <span style={{ color: 'red', fontSize: '12px' }}>
+                {errors.city.message}
+              </span>
+            )}
+          </FormControl>
+        </FlexRowContainer>
         <button type='submit'>Submit</button>
       </FlexColumnContainer>
     </AuthForm>
